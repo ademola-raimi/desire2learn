10d2c809d5eec35118dec175c55d193f88ad20f3
Raimi Ademola <ademola.raimi@andela.com>
2016-05-12 10:19:06 +0100

    [feature #118773189] modify the UI of the homepage

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -17,14 +17,54 @@
         }
     </style>
 
- <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet">
- <link href="{{ URL::asset('css/main.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('css/main.css') }}" rel="stylesheet">
+
+    <!-- Bootstrap CSS  -->
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" type="text/css">
+
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="{{ URL::asset('font-awesome/css/font-awesome.min.css') }}">
+
+    <!-- Owl Carousel CSS -->
+    <link rel="stylesheet" href="{{ URL::asset('css/owl.carousel.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/owl.theme.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/owl.transitions.css') }}">
+    
+    <!-- Css3 Transitions Styles  -->
+    <link rel="stylesheet" href="{{ URL::asset('css/animate.css') }}">
+    <!-- Lightbox CSS -->
+    <link rel="stylesheet" href="{{ URL::asset('css/lightbox.css') }}">
+
+    <!-- Sulfur CSS Styles  -->
+    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
+
+    <!-- Responsive CSS Style -->
+    <link rel="stylesheet" href="{{ URL::asset('css/responsive.css') }}">
+
+    <link rel="stylesheet" href="{{ URL::asset('js/modernizrr.js') }}">
 </head>
 <body>
 
     @yield('content')
     
-{{-- <script src="{{ elixir('js/app.js') }}"></script> --}} 
+   
+    <script src="{{ URL::asset('js/jquery-2.1.3.min.js') }}"></script>
+    <script src="{{ URL::asset('js/jquery-migrate-1.2.1.min.js') }}"></script>
+    <script src="{{ URL::asset('bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ URL::asset('js/owl.carousel.min.js') }}"></script>
+    <script src="{{ URL::asset('js/jquery.appear.js') }}"></script>
+    <script src="{{ URL::asset('js/jquery.fitvids.js') }}"></script>
+    <script src="{{ URL::asset('js/jquery.nicescroll.min.js') }}"></script>
+    <script src="{{ URL::asset('js/lightbox.min.js') }}"></script>
+    <script src="{{ URL::asset('js/count-to.js') }}"></script>
+    <script src="{{ URL::asset('js/styleswitcher.js') }}"></script>
+    <script src="{{ URL::asset('js/map.js') }}"></script>
+    <script src="{{ URL::asset('js/map.js') }}"></script>
+    
+    <script src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+    <script src="assets/js/script.js"></script> 
+        
 </body>
 </html>
 
