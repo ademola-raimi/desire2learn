332023a1228f9eadc820ba8ebd268e25070d2d96
Raimi Ademola <ademola.raimi@andela.com>
2016-05-12 12:18:09 +0100

    [feature #118773189] modify the UI of the homepage

--- resources/views/layout/partials/footer.blade.php
+++ resources/views/layout/partials/footer.blade.php
@@ -1,7 +1,25 @@
-<footer class="container-fluid">
-    <strong>Made with <span class="glyphicon glyphicon-heart" style="color: red;"></span>
-    <span class="incognito-text">By</span> <a href="https://github.com/andela-araimi" target="_blank">Demo</a></strong>
-    <span class="pull-right">
-        <strong> #TIA {{ \Carbon\Carbon::now()->year }}</strong>
-    </span>
-</footer>
+<!-- Start Copyright Section -->
+        <footer id="copyright-section" class="container-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-7">
+                        <div class="copyright">
+                        	<strong style="color: #fff"> #TIA {{ \Carbon\Carbon::now()->year }}.</strong>
+                        	<strong style="color: #fff">Made with <i class="fa fa-heart" style="color:red;"></i>
+    						<span class="incognito-text">By</span> <a href="https://github.com/andela-araimi" target="_blank">Demo</a></strong>
+                    	
+                        </div>
+                    </div>
+                    
+                    <div class="col-md-5">
+                        <div class="copyright-menu pull-right">
+                            <ul>
+                                <li><a href="/" class="active">Home</a></li>
+                                <li><a href="#">Sign Up</a></li>
+                                <li><a href="#">Login</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
