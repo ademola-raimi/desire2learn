8a04afdf7d820fd33734a0df1a9dd7c48bf95ae9
Raimi Ademola <ademola.raimi@andela.com>
2016-05-28 01:09:20 +0100

    [feature #119473043] modify migration files to fit sqlite for test purpose

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -17,6 +17,7 @@ $factory->define(Desire2Learn\User::class, function (Faker\Generator $faker) {
         'first_name'      => $faker->name,
         'last_name'       => $faker->name,
         'email'           => $faker->email,
+        'provider'        => $faker->name,
         'role_id'         => 1,
         'password'        => bcrypt(str_random(10)),
         'remember_token'  => str_random(10),
