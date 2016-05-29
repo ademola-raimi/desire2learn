46f489cdf180c4b2dde1be677a19646163fee5fa
Raimi Ademola <ademola.raimi@andela.com>
2016-05-29 17:55:29 +0100

    [feature #120497999] adjust something

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -32,28 +32,28 @@ class CommentController extends Controller
     /**
      * Get 10 comments from current video
      */
-    public function fetchComment(Request $request)
-    {
-        $totalComments = $request->input('offset');
-        $videoId       = $request->input('video_id');
+    // public function fetchComment(Request $request)
+    // {
+    //     $totalComments = $request->input('offset');
+    //     $videoId       = $request->input('video_id');
 
-        $oldComments = Comment::where('video_id', $videoId)
-            ->skip($totalComments)
-            ->take(10)
-            ->get();
-        if ($oldComments) {
-	        return $response =[
-	            'message' => 'Comment retrieved Successfully',
-	            'status_code' => 200,
-	            'comments' => $oldComments
-	        ];
-	    } else {
-	    	return $response = [
-	    		'message'     => 'something went wrong',
-	    		'status_code' => 404
-	    	];
-	    }    
-    }
+    //     $oldComments = Comment::where('video_id', $videoId)
+    //         ->skip($totalComments)
+    //         ->take(10)
+    //         ->get();
+    //     if ($oldComments) {
+	   //      return $response =[
+	   //          'message' => 'Comment retrieved Successfully',
+	   //          'status_code' => 200,
+	   //          'comments' => $oldComments
+	   //      ];
+	   //  } else {
+	   //  	return $response = [
+	   //  		'message'     => 'something went wrong',
+	   //  		'status_code' => 404
+	   //  	];
+	   //  }    
+    // }
 
     /**
      * deleteComment Delete a comment that belongs to the logged in user
@@ -61,19 +61,19 @@ class CommentController extends Controller
      * @param  $commentId
      * @return  
      */
-    public function deleteComment(Request $request, $commentId)
-    {
-        $request->session()->flash('show_comments', true);
+    // public function deleteComment(Request $request, $commentId)
+    // {
+    //     $request->session()->flash('show_comments', true);
 
-        $deleteComment = Comment::where('id', $commentId)
-        ->where('user_id', Auth::user()->id)
-        ->delete();
-        if ($deleteComment) {
-        	return $deleteComment;
-    	} else {
-    		alert()->error('Something went wrong', 'Error');
-    	}
-    }
+    //     $deleteComment = Comment::where('id', $commentId)
+    //     ->where('user_id', Auth::user()->id)
+    //     ->delete();
+    //     if ($deleteComment) {
+    //     	return $deleteComment;
+    // 	} else {
+    // 		alert()->error('Something went wrong', 'Error');
+    // 	}
+    // }
 
     /**
      * editComment, Allow an authenticated user to update the contents of a comment.
@@ -83,19 +83,19 @@ class CommentController extends Controller
      * @return boolean
      * 
      */
-    public function editComment(Request $request, $id)
-    {
-        $request->session()->flash('show_comments', true);
+    // public function editComment(Request $request, $id)
+    // {
+    //     $request->session()->flash('show_comments', true);
 
-        $updateComment = Comment::where('id', $id)
-        	->where('user_id', Auth::user()->id)
-        	->update([
-                'comments' => $request->input('comment')
-            ]);
-        if ($updateComment) {
-        	return $updateComment;
-        } else {
-        	alert()->error('Something went wrong', 'Error');
-        }
+    //     $updateComment = Comment::where('id', $id)
+    //     	->where('user_id', Auth::user()->id)
+    //     	->update([
+    //             'comments' => $request->input('comment')
+    //         ]);
+    //     if ($updateComment) {
+    //     	return $updateComment;
+    //     } else {
+    //     	alert()->error('Something went wrong', 'Error');
+    //     }
     }
 }
--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -80,7 +80,7 @@
 
     <script src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
     <script src="{!! load_asset('js/script.js') !!}"></script>
-    <script src="{!! load_asset('js/main.js') !!}"></script>
+    <script src="{!! load_asset('js/like.js') !!}"></script>
     <script src="{!! load_asset('js/comment.js') !!}"></script>
 </body>
 </html>
