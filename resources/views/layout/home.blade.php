1093a7bd3cc960b40c74ea7d02c735395e0e81fa
Raimi Ademola <ademola.raimi@andela.com>
2016-06-06 18:19:23 +0100

    fix unarrange video

--- resources/views/layout/home.blade.php
+++ resources/views/layout/home.blade.php
@@ -35,35 +35,38 @@
 	<div class="container">
 		<div class="col-sm-3 sidenav">@include('layout.partials.side-nav-bar')</div>
 		<div class="col-sm-9 sidebar">
-			@if (count($video) > 0)
-			@foreach ($video->chunk(3) as $chunk)
-			<div class="row">
-				@foreach ($chunk as $videos)
-				<div class="col-sm-3">
-					<div class="card-deck-wrapper">
-						<div class="card-deck sidebar-inner">
-							<div class="card" style="background-color: #fff;">
-								<a href="/video/{{ $videos->id }}">
-									<img src="http://img.youtube.com/vi/{{ $videos->url }}/0.jpg">
-								</a>
-								<div class="card-block">
-									<a class="card-title" style="width: 250px; overflow: hidden; text-overflow: ellipsis; " href="/video/{{ $videos->id }}">{{substr($videos->title, 0, 35) }} {{ strlen($videos->title) > 35 ? '...': ''}}</a>
-									<p class="card-text">Creator: {{ $videos->user->username }}</p>
-									<p class="card-text">{{ Carbon\Carbon::createFromTimeStamp(strtotime($videos->created_at))->diffForHumans() }}</p>
+			<div class="container">
+				@if (count($video) > 0)
+				@foreach ($video->chunk(3) as $chunk)
+				<div class="row">
+					@foreach ($chunk as $videos)
+					<div class="col-sm-3">
+						<div class="card-deck-wrapper">
+							<div class="card-deck sidebar-inner">
+								<div class="card" style="background-color: #fff;">
+									<a href="/video/{{ $videos->id }}">
+										<img src="http://img.youtube.com/vi/{{ $videos->url }}/0.jpg">
+									</a>
+									<div class="card-block">
+										<a class="card-title" style="width: 250px; overflow: hidden; text-overflow: ellipsis; " href="/video/{{ $videos->id }}">{{substr($videos->title, 0, 35) }} {{ strlen($videos->title) > 35 ? '...': ''}}</a>
+										<p class="card-text">Creator: {{ $videos->user->username }}</p>
+										<p class="card-text">{{ Carbon\Carbon::createFromTimeStamp(strtotime($videos->created_at))->diffForHumans() }}</p>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
+					@endforeach
 				</div>
 				@endforeach
+				@else
+				<h4 class="center-align padcast-page-header" style="margin-bottom:50px;">Oops sorry we have no videos yet</h4>
+				@endif
+				<div class="button-details">
+					{!! $video->render() !!}
+				</div>
 			</div>
-			@endforeach
-			@else
-			<h4 class="center-align padcast-page-header" style="margin-bottom:50px;">Oops sorry we have no videos yet</h4>
-			@endif
-			<div class="button-details">
-				{!! $video->render() !!}
-			</div>
+			
 		</div>
 	</div>
 	<!-- End Body Section -->
