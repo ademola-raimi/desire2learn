c785ec5e6c19f4fb1dae9e3495e30aabef4faab5
Raimi Ademola <ademola.raimi@andela.com>
2016-06-10 16:50:50 +0100

    [feature #120968577] Modify home.blade to make card responsive. Modify side.nav to leverage on bootstrap 4 panel for category display

--- resources/views/layout/partials/side-nav-bar.blade.php
+++ resources/views/layout/partials/side-nav-bar.blade.php
@@ -6,24 +6,21 @@
         <div class="panel-body popular-videos">
             <div class="row">
                 
-                <div class="row" >
-                    <div class="card card-inverse card-success" style="background-color: white; width: 80%; margin-left: 9%; border: 1px solid #e5e5e5">
-                        <div class="card-block bg-success" style="margin-bottom: 4%; background-color: #e5e5e5;">
-                        </div>
-                        <i style="width: 20em;" class="devicon-git-plain colored"></i>
+                
+                <div class="panel-body" >
+                        <i style="width: 20em; padding-left: 5%;" class="devicon-git-plain colored"></i>
                         <a class="text-uppercase" href="{{ route('show-all-category') }}">All</a>
-                    </div>
+                    
                     @foreach ($categories as $category)
-                    <div class="card card-inverse card-success" style="background-color: white; width: 80%; margin-left: 9%; border: 1px solid #e5e5e5">
-                        <div class="card-block bg-success" style="margin-bottom: 4%; background-color: #e5e5e5;">
-                        </div>
+                     <div class="panel-body" >
                         <i style="width: 20em;" class="devicon-{{ strtolower($category->name) }}-plain colored"></i>
                         <a class="text-uppercase" href="/category/{{ $category->id }}/videos">{{ $category->name }}</a>
-                    </div>
+                      </div>
                     @endforeach
                 </div>
-                
             </div>
+                
+         
         </div>
     </div>
 </div>
