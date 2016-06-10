53fcce55a566ae2b999bddf3bfd9088bfea20e78
Raimi Ademola <ademola.raimi@andela.com>
2016-06-10 19:12:40 +0100

    [feature #120968577] Modify the side navbar to have an awesome look

--- resources/views/layout/partials/side-nav-bar.blade.php
+++ resources/views/layout/partials/side-nav-bar.blade.php
@@ -7,12 +7,13 @@
             <div class="row">
                 
                 
-                <div class="panel-body" >
+                <div class="panel-body" style="margin-top: -8%;">
+                    <div class="row" style="margin-top: 2%; margin-left: -1.2%; margin-bottom: 6%">
                         <i style="width: 20em; padding-left: 5%;" class="devicon-git-plain colored"></i>
                         <a class="text-uppercase" href="{{ route('show-all-category') }}">All</a>
-                    
+                    </div>
                     @foreach ($categories as $category)
-                     <div class="panel-body" >
+                     <div class="panel-body" style="margin-top: -5%;">
                         <i style="width: 20em;" class="devicon-{{ strtolower($category->name) }}-plain colored"></i>
                         <a class="text-uppercase" href="/category/{{ $category->id }}/videos">{{ $category->name }}</a>
                       </div>
