8d0c597e6ee673ff5ce4105889ed86a8051241b1
Raimi Ademola <ademola.raimi@andela.com>
2016-06-13 15:44:12 +0100

    [feature #121298623] modify showvideo.blade.php to make related video view pleasant

--- resources/views/layout/partials/side-nav-bar.blade.php
+++ resources/views/layout/partials/side-nav-bar.blade.php
@@ -8,7 +8,7 @@
                 
                 
                 <div class="panel-body" style="margin-top: -8%;">
-                    <div class="row" style="margin-top: 2%; margin-left: -1.2%; margin-bottom: 6%">
+                    <div class="row" style="margin-top: 2%; margin-left: -1.2%; margin-bottom: 8%">
                         <i style="width: 20em; padding-left: 5%;" class="devicon-git-plain colored"></i>
                         <a class="text-uppercase" href="{{ route('show-all-category') }}">All</a>
                     </div>
